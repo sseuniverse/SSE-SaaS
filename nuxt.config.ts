cefd1703aad1064f8e1e4e645fc3209264728deb
SSE World <suryakantmaurya2007@gmail.com>
2024-07-21 21:26:58 +0530

    Commit 1

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  extends: ["@sse-ui/nuxt"],
+  typescript: { strict: false }, 
+  future: { compatibilityVersion: 4 },
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxthq/studio",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
+  ],
+
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    "components:extend": (components) => {
+      const globals = components.filter((c) =>
+        ["UButton"].includes(c.pascalName)
+      );
+
+      globals.forEach((c) => (c.global = true));
+    },
+  },
+
+  ui: {
+    icons: ["heroicons", "simple-icons"],
+  },
+
+  colorMode: {
+    disableTransition: true,
+  },
+
+  routeRules: {
+    // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
+    "/": { prerender: true },
+    "/api/search.json": { prerender: true },
+    "/docs": { redirect: "/docs/getting-started", prerender: false },
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
